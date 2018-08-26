ff1419cb66b56efc0393a6cde662eda565691043
Michelle Kim <pasolre95kim@gmail.com>
2018-08-26 23:19:20 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -55,13 +55,4 @@ end
 
 def checkout(cart, coupons)
 
-  grand_total = 0
-  cart = consolidate_cart(cart: cart)
-	  cart = apply_coupons(cart: cart, coupons: coupons)
-    cart = apply_clearance(cart: cart)
-    result = 0
-    cart.each do |food, info|
-      result += (info[:price] * info[:count]).to_f
-    end
-    result > 100 ? result * 0.9 : result
-  end
+  
